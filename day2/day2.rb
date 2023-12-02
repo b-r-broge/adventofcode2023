795c612a5aab1ded993d6a4c4c84e825bef18032
Brad Broge <brbroge@gmail.com>
2023-12-02 16:53:57 -0500

    day2 puzzle2.  This took longer than it should have because I accidentally corrupted the input file

--- day2/day2.rb
+++ day2/day2.rb
@@ -1,39 +1,27 @@
-# parse input, find if any of the values are greater than
-# puzzle 1
-# 12 red
-# 13 green
-# 14 blue
-# if all values are under -> add gameId to sum
-# if any value is over -> continue.
-
 sum = 0
-max_red = 12
-max_green = 13
-max_blue = 14
-
 File.foreach("input") { |line|
-  # Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-  is_valid = true
+  max_red = 1
+  max_green = 1
+  max_blue = 1
   game_line = line.sub('\n', '').split(': ')
   pulls = game_line[1].split('; ')
   pulls.each { |pull|
     cubes = pull.split(', ')
     cubes.each { |color| # '3 blue'
       values = color.split(' ')
+      cube_num = values[0].to_i
       case values[1]
       when 'red'
-        is_valid = values[0].to_i <= max_red
+        max_red = cube_num if cube_num > max_red
       when 'blue'
-        is_valid = values[0].to_i <= max_blue
+        max_blue = cube_num if cube_num > max_blue
       when 'green'
-        is_valid = values[0].to_i <= max_green
+        max_green = cube_num if cube_num > max_green
       end
-      break unless is_valid
     }
-    break unless is_valid
   }
-  # p line, is_valid
-  sum += game_line[0].split(' ')[1].to_i if is_valid
+  # puts line, max_red, max_blue, max_green
+  sum += (max_red*max_blue*max_green)
   # p sum
 }
 p sum
